4c7e6f260dde13d4d750e186c7b11b1bdaab5a10
Laerte Marçal <laerte.marcal@hotmail.com>
2014-06-17 22:56:59 -0300

    Escondidos RegID, SenderID e API Key por questões de segurança

--- aplicação servidor/src/br/ufg/integracao/envia/Config.java
+++ aplicação servidor/src/br/ufg/integracao/envia/Config.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class Config {
-	private static final String FILEPATH = "C:/Users/Laerte Filho/Desktop/config.ini";
+	private static final String FILEPATH = "assets/config.ini";
 	private static final Properties config = new Properties();
 	private static String data;
 
