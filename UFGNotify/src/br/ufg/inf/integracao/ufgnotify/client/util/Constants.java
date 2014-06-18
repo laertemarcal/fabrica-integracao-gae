18d59923fb7e5d40a62056d44c836ad3c0b46134
Laerte Marçal <laerte.marcal@hotmail.com>
2014-06-17 22:53:23 -0300

    Removido o SENDER_ID para proteção

--- UFGNotify/src/br/ufg/inf/integracao/ufgnotify/client/util/Constants.java
+++ UFGNotify/src/br/ufg/inf/integracao/ufgnotify/client/util/Constants.java
@@ -3,7 +3,7 @@ package br.ufg.inf.integracao.ufgnotify.client.util;
 public class Constants {
 	public static final String EXTRA_MESSAGE = "price";
 	public static final String PROPERTY_REG_ID = "registration_id";
-	public static final String SENDER_ID = "984742398302";
+	public static final String SENDER_ID = "SENDER_ID_DEVE_SER_COLOCADO_AQUI";
 	public static final String DISPLAY_MESSAGE_ACTION = "br.ufg.integracao.gcm.DISPLAY_MESSAGE";
 
 	public static final String PROPERTY_APP_VERSION = "appVersion";
